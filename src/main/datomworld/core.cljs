15296aea41aa22bbb03b6f39e97c95dd56a665c2
stzu <sonny.su@kaicode.com>
2021-04-24 17:26:22 -0400

    WIP

--- src/main/datomworld/core.cljs
+++ src/main/datomworld/core.cljs
@@ -1,5 +1,6 @@
 (ns datomworld.core
-  (:require [reagent.core :as r]
+  (:require [clojure.core.async :as a :include-macros true]
+            [reagent.core :as r]
             [reagent.dom :as rdom]
             ["ol" :as ol]
             ["ol/interaction" :as ol.interaction] 
@@ -10,13 +11,21 @@
             ["ol/proj" :as ol.proj :refer [fromLonLat]]
             ["ol/Feature" :default ol.Feature]
             ["ol/geom/Point" :default ol.geom.Point]
-            ["ol/style" :refer [Icon Style]]
-            ))
+            ["ol/style" :refer [Icon Style]]))
 
 (enable-console-print!)
 
+(def get-lon-lat (let [c (a/chan)]
+                   (fn []
+                     (.. js/navigator.geolocation (getCurrentPosition (fn [position]
+                                                                        (let [lon (.-longitude position.coords)
+                                                                              lat (.-latitude position.coords)]
+                                                                          (a/put! c [lon lat])))))
+                     c)))
+
 (def init-openlayer (let [full-screen? (atom false)]
                       (fn [{:keys [dom-id]}]
+
                         (let [rome (ol.Feature. #js{:geometry (ol.geom.Point. (fromLonLat #js[12.5, 41.9]))})
                               london (ol.Feature. #js{:geometry (ol.geom.Point. (fromLonLat #js[-0.12755, 51.507222]))})
                               red-square (Style. #js{:image (Icon. #js{:color "red"
@@ -28,26 +37,33 @@
                               
                               vector-source (VectorSource. #js{:features #js[rome london]})
                               vector-layer (ol.layer.VectorLayer. #js{:source vector-source})
-
-                              param (clj->js {:target dom-id
-                                              :layers #js[
-                                                          (ol.layer.Tile. #js{:source (ol.source/OSM.)})
-                                                          vector-layer
-                                                          ]
-                                              :view (ol/View. #js{:center (ol.proj/fromLonLat #js[109.22367 13.77648 ])
-                                                                  :zoom 0})
-                                              :interactions (ol.interaction/defaults #js{:doubleClickZoom false})})
-                              ol-map (ol/Map. param)]
-                          (.. ol-map (on "dblclick" (fn []
-                                                      (if @full-screen?
-                                                        (do
-                                                          (js/alert "Exit Full Screen")
-                                                          (js/document.exitFullscreen))
-                                                        (do
-                                                          (js/alert "Enter Full Screen")
-                                                          (js/document.documentElement.requestFullscreen)))
-                                                      (swap! full-screen? not))))
-                          ol-map))))
+                              lon-lat-ch (get-lon-lat)
+                              ]
+                          (a/go
+                            (let [[lon lat] (a/<! lon-lat-ch)
+                                  _ (prn "lon-lat=" [lon lat])
+                                  center (ol.proj/fromLonLat (clj->js [lon lat])  #_#js[109.22367 13.77648] #_#js[109.2236946 13.7519727])
+                                  foo  (ol/View. (clj->js {:center center
+                                                          :zoom 10}))
+                                  _ (prn "foo")
+                                  param #js{:target dom-id
+                                            :layers #js[(ol.layer.Tile. #js{:source (ol.source/OSM.)})
+                                                        vector-layer]
+                                            :view (ol/View. #js{:center (ol.proj/fromLonLat #_lon-lat  #js[109.22367 13.77648] #_#js[109.2236946 13.7519727])
+                                                                :zoom 10})
+                                            :interactions (ol.interaction/defaults #js{:doubleClickZoom false})}
+                                  ol-map (ol/Map. param)
+                                  ]
+                              (.. ol-map (on "dblclick" (fn []
+                                                          (if @full-screen?
+                                                            (do
+                                                              (js/alert "Exit Full Screen")
+                                                              (js/document.exitFullscreen))
+                                                            (do
+                                                              (js/alert "Enter Full Screen")
+                                                              (js/document.documentElement.requestFullscreen)))
+                                                          (swap! full-screen? not))))))
+                          ))))
 
 (defn init-materialize-ui []
   (js/M.AutoInit)
