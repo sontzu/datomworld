18a899a7dfa32caeee095880d80bdbb0d283d20c
stzu <sonny.su@kaicode.com>
2021-04-25 01:18:37 -0400

    mv inner let to outter let

--- src/main/datomworld/core.cljs
+++ src/main/datomworld/core.cljs
@@ -24,42 +24,41 @@
                                                  (a/put! c [lon lat])))))
                      c)))
 
-;;https://gis.stackexchange.com/questions/214400/dynamically-update-position-of-geolocation-marker-in-openlayers-3
-(def init-openlayer (let [full-screen? (atom false)]
-                      (fn [{:keys [dom-id]}]
 
-                        (let [rome (ol.Feature. #js{:geometry (ol.geom.Point. (fromLonLat #js[12.5, 41.9]))})
-                              london (ol.Feature. #js{:geometry (ol.geom.Point. (fromLonLat #js[-0.12755, 51.507222]))})
-                              red-square (Style. #js{:image (Icon. #js{:color "red"
-                                                                       :crossOrigin "anonymous"
-                                                                       :imgSize #js[20 20]
-                                                                       :src "https://openlayers.org/en/latest/examples/data/square.svg"})})
-                              _ (.setStyle rome red-square)
-                              _ (.setStyle london red-square)
-                              
-                              vector-source (VectorSource. #js{:features #js[rome london]})
-                              vector-layer (ol.layer.VectorLayer. #js{:source vector-source})
-                              lon-lat-ch (get-lon-lat)
-                              ]
-                          (a/go
-                            (let [lon-lat (a/<! lon-lat-ch)
-                                  param (clj->js {:target dom-id
-                                                 :layers #js[(ol.layer.Tile. #js{:source (ol.source/OSM.)})
-                                                             vector-layer]
-                                                 :view (ol/View. (clj->js {:center (ol.proj/fromLonLat (clj->js lon-lat))
-                                                                           :zoom 15}))
-                                                 :interactions (ol.interaction/defaults #js{:doubleClickZoom false})})
-                                  ol-map (ol/Map. param)]
-                              (.. ol-map (on "dblclick" (fn []
-                                                          (if @full-screen?
-                                                            (do
-                                                              (js/alert "Exit Full Screen")
-                                                              (js/document.exitFullscreen))
-                                                            (do
-                                                              (js/alert "Enter Full Screen")
-                                                              (js/document.documentElement.requestFullscreen)))
-                                                          (swap! full-screen? not))))))
-                          ))))
+;;https://gis.stackexchange.com/questions/214400/dynamically-update-position-of-geolocation-marker-in-openlayers-3
+(def init-openlayer (let [full-screen? (atom false)
+                          rome (ol.Feature. #js{:geometry (ol.geom.Point. (fromLonLat #js[12.5 41.9]))})
+                          london (ol.Feature. #js{:geometry (ol.geom.Point. (fromLonLat #js[-0.12755 51.507222]))})
+                          red-square (Style. #js{:image (Icon. #js{:color "red"
+                                                                   :crossOrigin "anonymous"
+                                                                   :imgSize #js[20 20]
+                                                                   :src "https://openlayers.org/en/latest/examples/data/square.svg"})})
+                          _ (.setStyle rome red-square)
+                          _ (.setStyle london red-square)
+                          
+                          vector-source (VectorSource. #js{:features #js[rome london]})
+                          vector-layer (ol.layer.VectorLayer. #js{:source vector-source})
+                          lon-lat-ch (get-lon-lat)]
+                      (fn [{:keys [dom-id]}]
+                        (a/go
+                          (let [lon-lat (a/<! lon-lat-ch)
+                                param (clj->js {:target dom-id
+                                                :layers #js[(ol.layer.Tile. #js{:source (ol.source/OSM.)})
+                                                            vector-layer]
+                                                :view (ol/View. (clj->js {:center (ol.proj/fromLonLat (clj->js lon-lat))
+                                                                          :zoom 15}))
+                                                :interactions (ol.interaction/defaults #js{:doubleClickZoom false})})
+                                ol-map (ol/Map. param)]
+                            (.. ol-map (on "dblclick" (fn []
+                                                        (if @full-screen?
+                                                          (do
+                                                            (js/alert "Exit Full Screen")
+                                                            (js/document.exitFullscreen))
+                                                          (do
+                                                            (js/alert "Enter Full Screen")
+                                                            (js/document.documentElement.requestFullscreen)))
+                                                        (swap! full-screen? not))))))
+                        )))
 
 (defn init-materialize-ui []
   (js/M.AutoInit)
